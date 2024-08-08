f5182822c8031e2470988969aa93dc5f7dbdf2ae
raskhan <raskanr.j07@gmail.com>
2024-08-09 00:41:43 +0530

    icon setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,8 +8,8 @@ import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
 import Octicons from '@expo/vector-icons/Octicons';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { DEFAULT_ICON_SIZE } from '@expo/vector-icons/build/createIconSet';
 
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
@@ -41,28 +41,28 @@ export default function TabLayout() {
         name="network"
         options={{
           title: 'Network',
-          tabBarIcon: ({ color,size }) => <MaterialIcons name="groups" size={37} color={color} />,
+          tabBarIcon: ({ color,size }) => <FontAwesome name="circle-o-notch" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="post"
         options={{
           title: 'Post',
-          tabBarIcon: ({ color,size }) => <Octicons name="diff-added" size={28} color={color} />,
+          tabBarIcon: ({ color,size }) => <FontAwesome name="plus-square-o" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="notification"
         options={{
           title: 'Notification',
-          tabBarIcon: ({ color,size }) => <Octicons name="bell-fill" size={28} color={color} />,
+          tabBarIcon: ({ color,size }) => <FontAwesome name="bell" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="jobs"
         options={{
           title: 'Jobs',
-          tabBarIcon: ({ color,size }) => <FontAwesome name="shopping-bag" size={28} color={color} />,
+          tabBarIcon: ({ color,size }) => <FontAwesome name="shopping-bag" size={size} color={color} />,
         }}
       />
     </Tabs>
